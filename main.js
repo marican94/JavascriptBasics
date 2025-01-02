c8314e8a82c828234d20b7c5aa15ccf259d21bf7
marican <muhammet.arican@xl2.de>
2025-01-02 17:27:26 +0100

    const and let Types

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 function testAlert(message) {
     alert(message)
 }
@@ -7,3 +8,22 @@ testAlert("Test 1 2 3")
 testAlert(4 + 5)
 
 console.log("Hello 2025")
+
+document.getElementById("greeting").innerHTML = "Hello World"
+*/
+
+document.querySelector("#open-nav-menu").addEventListener("click", function() {
+    document.querySelector("header nav .wrapper").classList.add("nav-open");    
+})
+
+document.querySelector("#close-nav-menu").addEventListener("click", function() {
+    document.querySelector("header nav .wrapper").classList.remove("nav-open");   
+})
+
+const customer = "John";
+let balance = 1000;
+console.log("Hello, "+ customer + " your balance is: " + balance);
+
+balance = balance + 200;
+
+console.log("Hello, "+ customer + " your new balance is: " + balance);
