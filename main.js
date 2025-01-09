c3c8131a2ae1eb222bf0a381200a871047b47015
marican <muhammet.arican@xl2.de>
2025-01-10 00:54:10 +0100

    New features

--- main.js
+++ main.js
@@ -10,11 +10,35 @@ document.querySelector("#close-nav-menu").addEventListener("click", function() {
 
 // Greeting Section
 
+function celsiusToFahr(temperature) {
+    let fahr = (temperature * 9/5) + 32;
+    return fahr;
+}
+
+celsiusToFahr(20);
+
 const greetingText = "Good afternoon!";
 const weatherCondition = "sunny";
 const userLocation = "Istanbul";
-let temperature = 22.8673;
-let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)}°C outside.`;
+let temperature = 25;
+let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)}°C outside.`;
+let fahrText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celsiusToFahr(temperature).toFixed(1)}°F outside.`;
 
 document.querySelector("#greeting").innerHTML = greetingText;
-document.querySelector("p#weather").innerHTML = weatherText;
+document.querySelector("p#weather").innerHTML = celsiusText;
+
+document.querySelector(".weather-group").addEventListener("click", function(e) {
+
+    if (e.target.id === "celsius") {
+        document.querySelector("p#weather").innerHTML = celsiusText;
+    } else if (e.target.id === "fahr") {
+        document.querySelector("p#weather").innerHTML = fahrText;
+    }    
+});
+
+setInterval(function(){
+    let localTime = new Date();
+    document.querySelector("span[data-time=hours]").innerHTML = localTime.getHours().toString().padStart(2, "0");
+    document.querySelector("span[data-time=minutes]").innerHTML = localTime.getMinutes().toString().padStart(2, "0");
+    document.querySelector("span[data-time=seconds]").innerHTML = localTime.getSeconds().toString().padStart(2, "0");
+}, 1000)
