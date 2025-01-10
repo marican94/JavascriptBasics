084eda2c6bbd240c1fc21663893183500a829951
marican <muhammet.arican@xl2.de>
2025-01-11 00:48:58 +0100

    image functions added

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 // Menu Section
 
-document.querySelector("#open-nav-menu").addEventListener("click", function() {
-    document.querySelector("header nav .wrapper").classList.add("nav-open");    
+document.querySelector("#open-nav-menu").addEventListener("click", function () {
+    document.querySelector("header nav .wrapper").classList.add("nav-open");
 })
 
-document.querySelector("#close-nav-menu").addEventListener("click", function() {
-    document.querySelector("header nav .wrapper").classList.remove("nav-open");   
+document.querySelector("#close-nav-menu").addEventListener("click", function () {
+    document.querySelector("header nav .wrapper").classList.remove("nav-open");
 })
 
 // Greeting Section
 
 function celsiusToFahr(temperature) {
-    let fahr = (temperature * 9/5) + 32;
+    let fahr = (temperature * 9 / 5) + 32;
     return fahr;
 }
 
@@ -27,18 +27,58 @@ let fahrText = `The weather is ${weatherCondition} in ${userLocation} and it's $
 document.querySelector("#greeting").innerHTML = greetingText;
 document.querySelector("p#weather").innerHTML = celsiusText;
 
-document.querySelector(".weather-group").addEventListener("click", function(e) {
+document.querySelector(".weather-group").addEventListener("click", function (e) {
 
     if (e.target.id === "celsius") {
         document.querySelector("p#weather").innerHTML = celsiusText;
     } else if (e.target.id === "fahr") {
         document.querySelector("p#weather").innerHTML = fahrText;
-    }    
+    }
 });
 
-setInterval(function(){
+setInterval(function () {
     let localTime = new Date();
     document.querySelector("span[data-time=hours]").innerHTML = localTime.getHours().toString().padStart(2, "0");
     document.querySelector("span[data-time=minutes]").innerHTML = localTime.getMinutes().toString().padStart(2, "0");
     document.querySelector("span[data-time=seconds]").innerHTML = localTime.getSeconds().toString().padStart(2, "0");
 }, 1000)
+
+
+//Gallery Section
+
+const galleryImages = [
+    {
+        src: "./assets/gallery/image1.jpg",
+        alt: "Thumbnail Image 1"
+    },
+    {
+        src: "./assets/gallery/image2.jpg",
+        alt: "Thumbnail Image 2"
+    },
+    {
+        src: "./assets/gallery/image3.jpg",
+        alt: "Thumbnail Image 3"
+    }
+];
+
+/* for (let i in galleryImages) {
+    console.log(galleryImages[i]);
+} */
+
+let mainImage = document.querySelector("#gallery > img");
+let thumbnails = document.querySelector("#gallery .thumbnails");
+mainImage.src = galleryImages[0].src;
+mainImage.alt = galleryImages[0].alt;
+
+//           <img src="./assets/gallery/image1.jpg" alt="Thumbnail Image 1" data-array-index="0" data-selected="true">
+
+
+galleryImages.forEach(function (image, index) {
+let thumb = document.createElement("img");
+thumb.src = image.src;
+thumb.alt = image.alt;
+thumb.dataset.arrayIndex = index;
+thumb.dataset.selected = false;
+console.log(thumb);
+thumbnails.appendChild(thumb);
+});
